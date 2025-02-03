9b325bd01e64fa97867eef8582ebb2d3de28a13a
Prattay9 <prattaydas9@gmail.com>
2025-02-03 15:51:51 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaYoutube } from "react-icons/fa";
+import "./Footer.css"; // Import the CSS file
+import logo from "../images/1.jpg";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-logo">
+          <img src={logo} alt="Sikharthy Infotech Logo" />
+        </div>
+        {/* About Section */}
+        <div className="footer-section">
+          <h2>About Elpis</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam id consectetur autem, aliquid necessitatibus, amet vel pariatur error atque est recusandae. Excepturi dolorem quo quibusdam omnis sit debitis vitae architecto?
+          </p>
+          <div className="social-icons">
+            <a href="#" className="social-icon youtube"><FaYoutube /></a>
+            <a href="#" className="social-icon facebook"><FaFacebookF /></a>
+            <a href="#" className="social-icon linkedin"><FaLinkedinIn /></a>
+            <a href="#" className="social-icon instagram"><FaInstagram /></a>
+          </div>
+        </div>
+
+        {/* Contact Details */}
+        <div className="footer-section">
+          <h2>Contact Details</h2>
+          <p>+91-8276-878-233</p>
+          <p>info@siplhub.com</p>
+        </div>
+
+        {/* Address */}
+        <div className="footer-section">
+          <h2>Address</h2>
+          <p>
+            PS SRIJAN CORPORATE PARK, Street Number 25, <br />
+            GP Block Tower 1, 11th Floor AKASA, Sector V, Bidhannagar, <br />
+            Kolkata, West Bengal 700091
+          </p>
+        </div>
+
+        {/* Instagram Posts */}
+        {/* <div className="footer-section">
+          <h2>Instagram Post</h2>
+          <div className="instagram-posts">
+            <div className="post"></div>
+            <div className="post"></div>
+            <div className="post"></div>
+            <div className="post"></div>
+            <div className="post"></div>
+            <div className="post"></div>
+          </div>
+        </div> */}
+      </div>
+
+      {/* Bottom Section */}
+      <div className="footer-bottom">
+        <p>© 2023 All Rights Reserved by Sikharthy Infotech</p>
+        <div className="footer-links">
+          <a href="#">Terms and Conditions</a>
+          <a href="#">Privacy Policy</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
